a39f963c5c034ae6ec1b0cfe0748a5242238c0df
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-17 17:03:59 +0000

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,18 @@
+class MusicImporter
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = Dir["#{path}/*.mp3"].collect{|i| i.gsub("#{path}/", "")}
+  end
+
+  def import
+    files.each{|filename| Song.create_from_filename(filename)}
+  end
+
+
+
+end
