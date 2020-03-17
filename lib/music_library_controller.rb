08463545119908a3bfa296cf27d2fd7ad51f463f
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-17 16:40:06 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -61,7 +61,13 @@ class MusicLibraryController
   end
 
   def list_songs_by_genre #takes user input for genre and returns that genres songs in an numbered list alphabetized by name
-
+    puts "Enter the name of an genre."
+    input = gets.strip
+    if genre = Genre.find_by_name(input)
+      artist.songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.name} - #{s.genre.name}"
+      end
+    end
   end
 
   def play_song #takes user input of a song number from #list_songs and puts "Playing #{song} by #{artist}".
